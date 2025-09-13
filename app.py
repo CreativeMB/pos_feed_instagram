fa6a9629fc7a1f969897793955e4a2255c1d1ba4
TobiasM <ingcpu@gmail.com>
2025-09-13 12:27:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -250,17 +250,17 @@ def trigger_manual_post():
 # -------------------------------
 # SCHEDULER CON CRON TRIGGER
 # -------------------------------
-scheduler = BlockingScheduler()
-scheduler.add_job(
-    tarea_programada_publicar_instagram,
-    trigger=CronTrigger(hour=12, minute=16, timezone=pytz.timezone("America/Bogota")),
-    id='instagram_daily_post',
-    name='Publicación diaria a las 12:10 PM (hora Colombia)',
-    replace_existing=True
-)
+#scheduler = BlockingScheduler()
+#scheduler.add_job(
+ #   tarea_programada_publicar_instagram,
+  #  trigger=CronTrigger(hour=12, minute=16, timezone=pytz.timezone("America/Bogota")),
+   # id='instagram_daily_post',
+    #name='Publicación diaria a las 12:10 PM (hora Colombia)',
+    #replace_existing=True
+#)
 
-print("Scheduler iniciado. La publicación diaria está programada a las 12:10 PM hora Colombia.")
-scheduler.start
+#print("Scheduler iniciado. La publicación diaria está programada a las 12:10 PM hora Colombia.")
+#scheduler.start
 
 # -------------------------------
 # INICIO DEL SERVIDOR FLASK
