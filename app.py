72aed011c9366aff90236cd4c6f09b91afd7c351
TobiasM <ingcpu@gmail.com>
2025-09-13 12:13:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
+from main_app import tarea_programada_publicar_instagram  # Importa la función
 import random
 import json
 import os
@@ -249,19 +250,17 @@ def trigger_manual_post():
 # -------------------------------
 # SCHEDULER CON CRON TRIGGER
 # -------------------------------
-scheduler = BackgroundScheduler()
-
-# Publicación diaria a las 12:10 PM hora Colombia
+scheduler = BlockingScheduler()
 scheduler.add_job(
     tarea_programada_publicar_instagram,
-    trigger=CronTrigger(hour=12, minute=10, timezone=pytz.timezone("America/Bogota")),
+    trigger=CronTrigger(hour=12, minute=16, timezone=pytz.timezone("America/Bogota")),
     id='instagram_daily_post',
     name='Publicación diaria a las 12:10 PM (hora Colombia)',
     replace_existing=True
 )
 
-scheduler.start()
 print("Scheduler iniciado. La publicación diaria está programada a las 12:10 PM hora Colombia.")
+scheduler.start
 
 # -------------------------------
 # INICIO DEL SERVIDOR FLASK
