62ddb02c503ae5a40286f96c4225ab1e705f5641
TobiasM <ingcpu@gmail.com>
2025-09-13 13:52:05 -0500

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,22 +1,15 @@
-# scheduler.py
+from main_app import tarea_programada_publicar_instagram
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
-from main_app import tarea_programada_publicar_instagram  # Importa la función desde app principal
 import pytz
 
-def iniciar_scheduler():
-    scheduler = BlockingScheduler()
-    
-    scheduler.add_job(
-        tarea_programada_publicar_instagram,
-        trigger=CronTrigger(hour=12, minute=45, timezone=pytz.timezone("America/Bogota")),
-        id='instagram_daily_post',
-        replace_existing=True
-    )
-    
-    print("Scheduler iniciado. Publicación diaria programada a las 12:45 PM hora Bogotá.")
-    scheduler.start()
+scheduler = BlockingScheduler()
 
+scheduler.add_job(
+    tarea_programada_publicar_instagram,
+    trigger=CronTrigger(hour=13, minute=55, timezone=pytz.timezone("America/Bogota")),
+    id='instagram_daily_post',
+    replace_existing=True
+)
 
-if __name__ == "__main__":
-    iniciar_scheduler()
+scheduler.start()
