00854ef54958cd30664a27d9611aae8186e06d83
TobiasM <ingcpu@gmail.com>
2025-09-13 16:21:29 -0500

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -8,18 +8,18 @@ scheduler = BlockingScheduler()
 # Job programado diario
 scheduler.add_job(
     tarea_programada_publicar_instagram,
-    trigger=CronTrigger(hour=15, minute=42, timezone=pytz.timezone("America/Bogota")),
+    trigger=CronTrigger(hour=16, minute=25, timezone=pytz.timezone("America/Bogota")),
     id='instagram_daily_post',
     replace_existing=True
 )
 
 # Job que se ejecuta inmediatamente al arrancar
-scheduler.add_job(
-    tarea_programada_publicar_instagram,
-    trigger='date',  # Se ejecuta solo una vez
-    id='instagram_immediate_post',
-    replace_existing=True
-)
+#scheduler.add_job(
+  #  tarea_programada_publicar_instagram,
+   # trigger='date',  # Se ejecuta solo una vez
+  #  id='instagram_immediate_post',
+  #  replace_existing=True
+#)
 
 print("Scheduler iniciado. Publicación diaria programada y disparo inmediato en curso.")
 scheduler.start()
