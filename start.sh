b37212e4488d398cf5bfa22a071eeb7a6ee2b715
TobiasM <ingcpu@gmail.com>
2025-09-16 14:24:40 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# Ejecutar cron en primer plano y enviar todo a stdout para ver logs
-supercronic /app/crontab >> /proc/1/fd/1 2>&1 &
+# Ejecutar Flask en segundo plano
+python app.py &
 
-# Ejecutar Flask en primer plano
-exec python app.py
+# Ejecutar Supercronic en primer plano (logs aparecerán en fly logs)
+exec supercronic /app/crontab
