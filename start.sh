2456aff5d0012a719e0405103692f9384b400e88
TobiasM <ingcpu@gmail.com>
2025-09-16 14:17:25 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# Ejecutar supercronic en segundo plano
-supercronic /app/crontab &
+# Ejecutar cron en primer plano y enviar todo a stdout para ver logs
+supercronic /app/crontab >> /proc/1/fd/1 2>&1 &
+
 # Ejecutar Flask en primer plano
 exec python app.py
